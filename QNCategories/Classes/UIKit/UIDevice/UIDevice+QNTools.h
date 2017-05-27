07875b7d8f35a154af7d29be64fa2ae4c65c8e0b
gitph101 <850867354@qq.com>
2017-05-27 11:22:57 +0800

    sss

--- QNCategories/Classes/UIKit/UIDevice/UIDevice+QNTools.h
+++ QNCategories/Classes/UIKit/UIDevice/UIDevice+QNTools.h
@@ -36,6 +36,61 @@ NS_ASSUME_NONNULL_BEGIN
 /// 获取手机硬盘空闲空间, 返回的是字节数
 + (long long)totalDiskSpaceBytes;
 
+
+#pragma mark - Memory Information
+///=============================================================================
+/// @name Memory Information
+///=============================================================================
+
+/// Total physical memory in byte. (-1 when error occurs)
+@property (nonatomic, readonly) int64_t memoryTotal;
+
+/// Used (active + inactive + wired) memory in byte. (-1 when error occurs)
+@property (nonatomic, readonly) int64_t memoryUsed;
+
+/// Free memory in byte. (-1 when error occurs)
+@property (nonatomic, readonly) int64_t memoryFree;
+
+/// Acvite memory in byte. (-1 when error occurs)
+@property (nonatomic, readonly) int64_t memoryActive;
+
+/// Inactive memory in byte. (-1 when error occurs)
+@property (nonatomic, readonly) int64_t memoryInactive;
+
+/// Wired memory in byte. (-1 when error occurs)
+@property (nonatomic, readonly) int64_t memoryWired;
+
+/// Purgable memory in byte. (-1 when error occurs)
+@property (nonatomic, readonly) int64_t memoryPurgable;
+
+///=============================================================================
+/// @name CPU Information
+///=============================================================================
+
+/// Avaliable CPU processor count.
+@property (nonatomic, readonly) NSUInteger cpuCount;
+
+/// Current CPU usage, 1.0 means 100%. (-1 when error occurs)
+@property (nonatomic, readonly) float cpuUsage;
+
+/// Current CPU usage per processor (array of NSNumber), 1.0 means 100%. (nil when error occurs)
+@property (nullable, nonatomic, readonly) NSArray<NSNumber *> *cpuUsagePerProcessor;
+
+#pragma mark - Disk Space
+///=============================================================================
+/// @name Disk Space
+///=============================================================================
+
+/// Total disk space in byte. (-1 when error occurs)
+@property (nonatomic, readonly) int64_t diskSpace;
+
+/// Free disk space in byte. (-1 when error occurs)
+@property (nonatomic, readonly) int64_t diskSpaceFree;
+
+/// Used disk space in byte. (-1 when error occurs)
+@property (nonatomic, readonly) int64_t diskSpaceUsed;
+
+
 @end
 
 NS_ASSUME_NONNULL_END
--- QNCategories/Classes/UIKit/UIDevice/UIDevice+QNTools.m
+++ QNCategories/Classes/UIKit/UIDevice/UIDevice+QNTools.m
@@ -211,6 +211,7 @@
 }
 
 #pragma mark - memory information
+
 + (NSUInteger)cpuFrequency {
     return [self getSysInfo:HW_CPU_FREQ];
 }
@@ -248,8 +249,194 @@
     return mem_free;
 }
 
+///memory
+- (int64_t)memoryTotal {
+    int64_t mem = [[NSProcessInfo processInfo] physicalMemory];
+    if (mem < -1) mem = -1;
+    return mem;
+}
+
+- (int64_t)memoryUsed {
+    mach_port_t host_port = mach_host_self();
+    mach_msg_type_number_t host_size = sizeof(vm_statistics_data_t) / sizeof(integer_t);
+    vm_size_t page_size;
+    vm_statistics_data_t vm_stat;
+    kern_return_t kern;
+    
+    kern = host_page_size(host_port, &page_size);
+    if (kern != KERN_SUCCESS) return -1;
+    kern = host_statistics(host_port, HOST_VM_INFO, (host_info_t)&vm_stat, &host_size);
+    if (kern != KERN_SUCCESS) return -1;
+    return page_size * (vm_stat.active_count + vm_stat.inactive_count + vm_stat.wire_count);
+}
+
+- (int64_t)memoryFree {
+    mach_port_t host_port = mach_host_self();
+    mach_msg_type_number_t host_size = sizeof(vm_statistics_data_t) / sizeof(integer_t);
+    vm_size_t page_size;
+    vm_statistics_data_t vm_stat;
+    kern_return_t kern;
+    
+    kern = host_page_size(host_port, &page_size);
+    if (kern != KERN_SUCCESS) return -1;
+    kern = host_statistics(host_port, HOST_VM_INFO, (host_info_t)&vm_stat, &host_size);
+    if (kern != KERN_SUCCESS) return -1;
+    return vm_stat.free_count * page_size;
+}
+
+- (int64_t)memoryActive {
+    mach_port_t host_port = mach_host_self();
+    mach_msg_type_number_t host_size = sizeof(vm_statistics_data_t) / sizeof(integer_t);
+    vm_size_t page_size;
+    vm_statistics_data_t vm_stat;
+    kern_return_t kern;
+    
+    kern = host_page_size(host_port, &page_size);
+    if (kern != KERN_SUCCESS) return -1;
+    kern = host_statistics(host_port, HOST_VM_INFO, (host_info_t)&vm_stat, &host_size);
+    if (kern != KERN_SUCCESS) return -1;
+    return vm_stat.active_count * page_size;
+}
+
+- (int64_t)memoryInactive {
+    mach_port_t host_port = mach_host_self();
+    mach_msg_type_number_t host_size = sizeof(vm_statistics_data_t) / sizeof(integer_t);
+    vm_size_t page_size;
+    vm_statistics_data_t vm_stat;
+    kern_return_t kern;
+    
+    kern = host_page_size(host_port, &page_size);
+    if (kern != KERN_SUCCESS) return -1;
+    kern = host_statistics(host_port, HOST_VM_INFO, (host_info_t)&vm_stat, &host_size);
+    if (kern != KERN_SUCCESS) return -1;
+    return vm_stat.inactive_count * page_size;
+}
+
+- (int64_t)memoryWired {
+    mach_port_t host_port = mach_host_self();
+    mach_msg_type_number_t host_size = sizeof(vm_statistics_data_t) / sizeof(integer_t);
+    vm_size_t page_size;
+    vm_statistics_data_t vm_stat;
+    kern_return_t kern;
+    
+    kern = host_page_size(host_port, &page_size);
+    if (kern != KERN_SUCCESS) return -1;
+    kern = host_statistics(host_port, HOST_VM_INFO, (host_info_t)&vm_stat, &host_size);
+    if (kern != KERN_SUCCESS) return -1;
+    return vm_stat.wire_count * page_size;
+}
+
+- (int64_t)memoryPurgable {
+    mach_port_t host_port = mach_host_self();
+    mach_msg_type_number_t host_size = sizeof(vm_statistics_data_t) / sizeof(integer_t);
+    vm_size_t page_size;
+    vm_statistics_data_t vm_stat;
+    kern_return_t kern;
+    
+    kern = host_page_size(host_port, &page_size);
+    if (kern != KERN_SUCCESS) return -1;
+    kern = host_statistics(host_port, HOST_VM_INFO, (host_info_t)&vm_stat, &host_size);
+    if (kern != KERN_SUCCESS) return -1;
+    return vm_stat.purgeable_count * page_size;
+}
+
+
+#pragma mark - cpu information
+
+- (NSUInteger)cpuCount {
+    return [NSProcessInfo processInfo].activeProcessorCount;
+}
+
+- (float)cpuUsage {
+    float cpu = 0;
+    NSArray *cpus = [self cpuUsagePerProcessor];
+    if (cpus.count == 0) return -1;
+    for (NSNumber *n in cpus) {
+        cpu += n.floatValue;
+    }
+    return cpu;
+}
+
+- (NSArray *)cpuUsagePerProcessor {
+    processor_info_array_t _cpuInfo, _prevCPUInfo = nil;
+    mach_msg_type_number_t _numCPUInfo, _numPrevCPUInfo = 0;
+    unsigned _numCPUs;
+    NSLock *_cpuUsageLock;
+    
+    int _mib[2U] = { CTL_HW, HW_NCPU };
+    size_t _sizeOfNumCPUs = sizeof(_numCPUs);
+    int _status = sysctl(_mib, 2U, &_numCPUs, &_sizeOfNumCPUs, NULL, 0U);
+    if (_status)
+        _numCPUs = 1;
+    
+    _cpuUsageLock = [[NSLock alloc] init];
+    
+    natural_t _numCPUsU = 0U;
+    kern_return_t err = host_processor_info(mach_host_self(), PROCESSOR_CPU_LOAD_INFO, &_numCPUsU, &_cpuInfo, &_numCPUInfo);
+    if (err == KERN_SUCCESS) {
+        [_cpuUsageLock lock];
+        
+        NSMutableArray *cpus = [NSMutableArray new];
+        for (unsigned i = 0U; i < _numCPUs; ++i) {
+            Float32 _inUse, _total;
+            if (_prevCPUInfo) {
+                _inUse = (
+                          (_cpuInfo[(CPU_STATE_MAX * i) + CPU_STATE_USER]   - _prevCPUInfo[(CPU_STATE_MAX * i) + CPU_STATE_USER])
+                          + (_cpuInfo[(CPU_STATE_MAX * i) + CPU_STATE_SYSTEM] - _prevCPUInfo[(CPU_STATE_MAX * i) + CPU_STATE_SYSTEM])
+                          + (_cpuInfo[(CPU_STATE_MAX * i) + CPU_STATE_NICE]   - _prevCPUInfo[(CPU_STATE_MAX * i) + CPU_STATE_NICE])
+                          );
+                _total = _inUse + (_cpuInfo[(CPU_STATE_MAX * i) + CPU_STATE_IDLE] - _prevCPUInfo[(CPU_STATE_MAX * i) + CPU_STATE_IDLE]);
+            } else {
+                _inUse = _cpuInfo[(CPU_STATE_MAX * i) + CPU_STATE_USER] + _cpuInfo[(CPU_STATE_MAX * i) + CPU_STATE_SYSTEM] + _cpuInfo[(CPU_STATE_MAX * i) + CPU_STATE_NICE];
+                _total = _inUse + _cpuInfo[(CPU_STATE_MAX * i) + CPU_STATE_IDLE];
+            }
+            [cpus addObject:@(_inUse / _total)];
+        }
+        
+        [_cpuUsageLock unlock];
+        if (_prevCPUInfo) {
+            size_t prevCpuInfoSize = sizeof(integer_t) * _numPrevCPUInfo;
+            vm_deallocate(mach_task_self(), (vm_address_t)_prevCPUInfo, prevCpuInfoSize);
+        }
+        return cpus;
+    } else {
+        return nil;
+    }
+}
+
+
 #pragma mark - disk information
 
+
+
+- (int64_t)diskSpace {
+    NSError *error = nil;
+    NSDictionary *attrs = [[NSFileManager defaultManager] attributesOfFileSystemForPath:NSHomeDirectory() error:&error];
+    if (error) return -1;
+    int64_t space =  [[attrs objectForKey:NSFileSystemSize] longLongValue];
+    if (space < 0) space = -1;
+    return space;
+}
+
+- (int64_t)diskSpaceFree {
+    NSError *error = nil;
+    NSDictionary *attrs = [[NSFileManager defaultManager] attributesOfFileSystemForPath:NSHomeDirectory() error:&error];
+    if (error) return -1;
+    int64_t space =  [[attrs objectForKey:NSFileSystemFreeSize] longLongValue];
+    if (space < 0) space = -1;
+    return space;
+}
+
+- (int64_t)diskSpaceUsed {
+    int64_t total = self.diskSpace;
+    int64_t free = self.diskSpaceFree;
+    if (total < 0 || free < 0) return -1;
+    int64_t used = total - free;
+    if (used < 0) used = -1;
+    return used;
+}
+
+///
 + (long long)freeDiskSpaceBytes
 {
     struct statfs buf;
